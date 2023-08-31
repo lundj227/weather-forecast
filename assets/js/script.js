bdcf9c24a558872fd3f7d641ec34f447be6d54fc
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-08-31 16:44:37 -0700

    Added local storage functionality and a history of cities searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,8 @@ var date;
 var wind;
 var cityNameFromServer;
 var humidity;
-var currentConditionsDiv = document.querySelector('#current-conditions');
-var futureConditionsDiv = document.querySelector('#future-conditions');
+var currentConditionsDiv = document.querySelector('#current-conditions-display');
+var futureConditionsDiv = document.querySelector('#future-conditions-display');
 
 function formatUnixTime(unixTimestamp) {
     const date = new Date(unixTimestamp * 1000); // Convert to milliseconds
@@ -22,19 +22,19 @@ function formatUnixTime(unixTimestamp) {
     return `${month}/${day}/${year}`;
 }
 
-function getCoordinates(){
+var searchedCities = [];
+
+function getCoordinates() {
     city = cityInput.value;
     fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + APIKey)
-        .then(function(response){
+        .then(function (response) {
             return response.json();
         })
-        .then(function (data){
+        .then(function (data) {
             lat = data[0].lat;
             lon = data[0].lon;
-        })
-        .then(function(){
-            weatherQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ `${lat}` + '&lon=' + `${lon}` + '&appid=' +  `${APIKey}` + '&units=imperial';
-            getFutureWeather();
+            weatherQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey + '&units=imperial';
+            getFutureWeather(); // Move getFutureWeather() call here
         });
 }
 
@@ -46,54 +46,60 @@ function getFutureWeather(){
     .then(function(data){
         futureConditionsDiv.innerHTML = '';
         cityNameFromServer = data.city.name // gets the name
-        console.log(cityNameFromServer);
-        console.log(data);
 
-        var containerDiv = document.createElement('div'); // Create a container div
         var futureWeathercard = document.createElement('div');
         // Gets the date, temp, wind, and humidity of a city
+        var futureWeatherList = [];
+        var futureWeatherObject = {};
         for(var i = 8; i < 40; i += 8){ // gets the next five days at midnight
             
-            // Save the date as a variable and format it
-            date = formatUnixTime(data.list[i].dt); 
-            //console.log(date); 
-
-            // Save the temperature as a variable and format it
-            tempF = data.list[i].main.temp;
-            tempF = tempF + " °F" ;
-            //console.log(tempF);
-
-            // Save the wind data as a variable and format it
+            // Save the date, temp, wind, and humidity as a variable and format it
+            date = formatUnixTime(data.list[i].dt);
+            tempF = data.list[i].main.temp + " °F";
             wind = data.list[i].wind.speed + ' MPH';
-            //console.log(wind);
-
-            // Save the humidity data as a variable and format it
             humidity = data.list[i].main.humidity + '%';
-            //console.log(humidity);
 
+            futureWeatherList = [tempF, wind, humidity];
+
+            if (!futureWeatherObject[cityNameFromServer]){
+                futureWeatherObject[cityNameFromServer] = {};
+            }
+
+            futureWeatherObject[cityNameFromServer][date] = futureWeatherList;
             // Create the cards for forecasted weather 
-            //var futureWeathercard = document.createElement('div');
+            var futureWeathercard = document.createElement('div');
             futureWeathercard = document.createElement('div');
             futureWeathercard.classList.add('future-weather-card');
             futureWeathercard.innerHTML = `<p> ${date} <br>Temp: ${tempF}<br>Wind: ${wind}<br>Humidity: ${humidity}<br>`
             futureConditionsDiv.appendChild(futureWeathercard);
+
+            localStorage.setItem(`${cityNameFromServer}`, JSON.stringify(futureWeatherObject));
         }        
         // Now outside for loop
-        // Save the date as a variable and format it
+
+        var futureWeatherString = '';
+        for (var date in futureWeatherObject[cityNameFromServer]) {
+            futureWeatherString += `${date}: ${futureWeatherObject[cityNameFromServer][date].join(', ')}<br>`;
+        }
+
+        // Store the combined future weather data string in local storage
+        var storedCityData = localStorage.getItem(cityNameFromServer);
+        var cityData = storedCityData ? JSON.parse(storedCityData) : {};
+        cityData.future = futureWeatherString;
+        localStorage.setItem(cityNameFromServer, JSON.stringify(cityData));
+        // Save the date, temp, wind, and humidity as a variable and format it
+         // For whatever reason it did not want to create all 5 future days so the last one I created manually
         var dateLast = formatUnixTime(data.list[38].dt); 
-        
-        var tempFLast = data.list[38].main.temp;
-        tempFLast = tempF + " °F" ;
-        
-        // Save the wind data as a variable and format it
+        var tempFLast = data.list[38].main.temp + " °F";
         var windLast = data.list[38].wind.speed + ' MPH';
-        // Save the humidity data as a variable and format it
         var humidityLast = data.list[38].main.humidity + '%';
-        // For whatever reason it did not want to create all 5 future days so the last one I created manually
+
+        
+    
         futureWeathercard = document.createElement('div');
         futureWeathercard.classList.add('future-weather-card');
         futureWeathercard.innerHTML = `<p> ${dateLast} <br>Temp: ${tempFLast}<br>Wind: ${windLast}<br>Humidity: ${humidityLast}<br>`
-        futureConditionsDiv.appendChild(futureWeathercard);
+        futureConditionsDiv.appendChild(futureWeathercard);    
     })
 };
 
@@ -106,7 +112,6 @@ function getCurrentWeather() {
             return response.json();
         })
         .then(function(data){
-            console.log(data);
 
             // Extract relevant data from the response
             var tempF = data.main.temp + " °F";
@@ -130,9 +135,35 @@ function getCurrentWeather() {
         });
 }
 
-searchBtn.addEventListener('click', function(e){
+function displayWeatherData(cityName) {
+    var storedCityData = localStorage.getItem(cityName);
+    var cityData = storedCityData ? JSON.parse(storedCityData) : {};
+
+    // Display current weather conditions
+    var currentConditionsDisplay = document.querySelector('#current-conditions-display');
+    currentConditionsDisplay.innerHTML = `
+        <h3>${cityName.toUpperCase()} ${cityData.date}</h3><br>
+        Temp: ${cityData.tempF}<br>
+        Wind: ${cityData.wind}<br>
+        Humidity: ${cityData.humidity}<br>
+    `;
+    currentConditionsDisplay.appendChild(futureWeatherCard);
+}
+
+searchBtn.addEventListener('click', function (e) {
     e.preventDefault();
     city = cityInput.value;
     getCurrentWeather();
-    getCoordinates(); 
+    getCoordinates();
+
+    if (!searchedCities.includes(city)) { // Check for duplicate city
+        searchedCities.push(city); // Add city to the array
+        var cityHistorySection = document.querySelector('#cityHistory');
+        var cityButton = document.createElement('button');
+        cityButton.textContent = city.toUpperCase();
+        cityButton.addEventListener('click', function () {
+            displayWeatherData(city);
+        });
+        cityHistorySection.appendChild(cityButton);
+    }
 });
